9c3eebadf76a1fb6256f0d2b21fea865f8f7df9c
Mohammed-Zeghlache <mouhamedzeghlache7@gmail.com>
2026-07-09 19:30:37 +0200

    Update .puppeteerrc.js

--- .puppeteerrc.js
+++ .puppeteerrc.js
@@ -4,6 +4,6 @@ const { join } = require('path');
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
-  // Changes the cache location for Puppeteer
-  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+  // Use absolute path
+  cacheDirectory: join('/opt/render/project/src', '.cache', 'puppeteer'),
 };
