c05af8014627037d0501a9a65b09b99271050052
Mohammed-Zeghlache <mouhamedzeghlache7@gmail.com>
2026-07-10 16:40:22 +0200

    Update .puppeteerrc.js

--- .puppeteerrc.js
+++ .puppeteerrc.js
@@ -1,9 +1,17 @@
+// const { join } = require('path');
+
+// /**
+//  * @type {import("puppeteer").Configuration}
+//  */
+// module.exports = {
+//   // Use absolute path
+//   cacheDirectory: join('/opt/render/project/src', '.cache', 'puppeteer'),
+// };
+
+
+
 const { join } = require('path');
 
-/**
- * @type {import("puppeteer").Configuration}
- */
 module.exports = {
-  // Use absolute path
-  cacheDirectory: join('/opt/render/project/src', '.cache', 'puppeteer'),
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
